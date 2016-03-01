b541d217468c996afefa595779929fec02e289e7
Bojan Stefanovic <bojanstef4@gmail.com>
2016-02-29 22:52:37 -0800

    Updated port for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const middleware = require('./middleware.js');
 // app.get('/', function(request, response) {
 // 	response.send('Hello Express!');
