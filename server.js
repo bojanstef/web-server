6b503e572ca9324623ca5c6bc28fcc666526e307
Bojan Stefanovic <bojanstef4@gmail.com>
2016-02-29 22:36:50 -0800

    Abstracted out middleware

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const middleware = require('./middleware.js');
 // app.get('/', function(request, response) {
 // 	response.send('Hello Express!');
 // });
-
-var middleware = {
-	requireAuthentication: function(request, response, next) {
-		console.log('Private route hit!');
-		next();
-	},
-	logger: function(request, response, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + request.method + ' ' + request.originalUrl);
-		next();
-	}
-};
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
